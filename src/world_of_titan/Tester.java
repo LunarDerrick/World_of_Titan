83db153849cf08dc457504a77a78412cc0227fb9
Derrick <derrickphb@gmail.com>
2022-05-14 16:43:07 +0800

    [Phang]Update 14 May 2022: - create map & mapNode to store data

--- src/world_of_titan/Tester.java
+++ src/world_of_titan/Tester.java
@@ -1,4 +1,7 @@
 package world_of_titan;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Tester {
 
@@ -8,6 +11,30 @@ public class Tester {
         Scouting scouting = new Scouting();
         SoldierArrange soldierArrange = new SoldierArrange();
         Titan titan = new Titan();
+        
+        // Load data from data.txt
+        try {
+            Scanner read = new Scanner(new FileInputStream("data.txt"));
+            while (read.hasNextLine()) {
+                String input = read.nextLine();
+                String[] inputList = input.split(" ");
+                int[] numberList = new int[inputList.length];
+                
+                for (int i = 0; i < numberList.length; i++)
+                    numberList[i] = Integer.parseInt(inputList[i]);
+                
+                int[] values = new int[numberList.length-2];
+                System.arraycopy(numberList, 2, values, 0, numberList.length-2);
+                
+                scouting.mapNode(numberList[0], numberList[1], values);
+            }
+            
+            read.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        
+        System.out.println(scouting.toString());
     }
 
 }
